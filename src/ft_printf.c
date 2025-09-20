105e8b8bff6b39be820aa4c0e9dcc59bfc078228
thribeir <thiagoribeirochico@gmail.com>
2025-09-20 21:31:46 +0100

    util apply precision implemented

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,35 +6,33 @@
 /*   By: thribeir <thribeir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 23:36:05 by thribeir          #+#    #+#             */
-/*   Updated: 2025/09/20 18:57:39 by thribeir         ###   ########.fr       */
+/*   Updated: 2025/09/20 19:27:14 by thribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	print_type(char c, va_list args)
+int	print_type(t_format *fmt, va_list args)
 {
-	if (c == 'd' || c == 'i')
-		return (print_int(args));
-	else if (c == 'c')
-		return (print_char(args));
-	else if (c == 's')
-		return (print_str(args));
-	else if (c == 'p')
-		return (print_ptr(args));
-	else if (c == 'u')
-		return (print_unint(args));
-	else if (c == 'x')
-		return (print_hex(args, "0123456789abcdef"));
-	else if (c == 'X')
-		return (print_hex(args, "0123456789ABCDEF"));
-	else if (c == '%')
-	{
-		write(1, &c, 1);
-		return (1);
-	}
+	if (fmt -> spec == 'd' || fmt -> spec == 'i')
+		return (print_int(args, fmt));
+	else if (fmt -> spec == 'c')
+		return (print_char(args, fmt, 0));
+	else if (fmt -> spec == 's')
+		return (print_str(args, fmt));
+	else if (fmt -> spec == 'p')
+		return (print_ptr(args, fmt));
+	else if (fmt -> spec == 'u')
+		return (print_unint(args, fmt));
+	else if (fmt -> spec == 'x')
+		return (print_hex(args, "0123456789abcdef", fmt));
+	else if (fmt -> spec == 'X')
+		return (print_hex(args, "0123456789ABCDEF", fmt));
+	else if (fmt -> spec == '%')
+		return (print_char(args, fmt, 1));
 	return (0);
 }
+
 
 int	ft_printf(const char *format, ...)
 {
